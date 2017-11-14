193f195d6d66b28b55fb74a1e99d3b1a10fed8b9
JacekMistrzuKoder <jacek301@gmail.com>
2017-11-14 23:07:29 +0100

    Improvment if is my list then last track switch to 2 element otherwise it stays as selected

--- mp3u8ListMaker.py
+++ mp3u8ListMaker.py
@@ -1,41 +1,52 @@
 import sys
 import os
 
-# print('arguments: ', len(sys.argv))
-# print('argument list: ', str(sys.argv))
-# print('argument list: ', sys.argv[1])
+def check_if_before_are_numbers_and_last_is_letter(listOfFirstTrackLetters):
 
-listBegin = '#EXTM3U'
-nextTrackBeginOnTop = '#EXTINF:'
+    if  listOfFirstTrackLetters[-1].isdigit():
+        return False
 
-listContent='';
+    for trackFirstLetter in listOfFirstTrackLetters[:-1]:
+        if not trackFirstLetter.isdigit():
+            return False
 
-listContent += listBegin + '\n'
-# listContent += nextTrackBeginOnTop + '\n'
+    return True
+
+M3U8StartTag = '#EXTM3U'
+M3U8StartTrackTag = '#EXTINF:'
+CONST_FIRST_TRACK_LETTER_POSITION = 9
+
+listOfTracks = []
+listOfFirstTrackLetters = []
+
+oneTrackEntry= '';
+listOfTracks.append(M3U8StartTag + '\n')
+
 
 for index, path in enumerate(sys.argv,start=0):
-     if(index != 0):
-       listContent += nextTrackBeginOnTop + '\n'
-       listContent += os.path.basename(sys.argv[index]) + '\n'
+    if index != 0:
+        oneTrackEntry += M3U8StartTrackTag + '\n'
+        oneTrackEntry += os.path.basename(sys.argv[index]) + '\n'
+        listOfFirstTrackLetters.append(oneTrackEntry[CONST_FIRST_TRACK_LETTER_POSITION])
+        listOfTracks.append(oneTrackEntry)
+        oneTrackEntry = ''
 
+if check_if_before_are_numbers_and_last_is_letter(listOfFirstTrackLetters):
+
+    lastTrack = listOfTracks.pop(-1)
+    listOfTracks.insert(1, lastTrack)
+
+finalListTrack = ''.join(listOfTracks)
 
 files_dir_path = os.path.dirname(sys.argv[1])
-#print(files_dir_path)
-
 m3u8ListFullName = files_dir_path+ '\\'
 m3u8ListName = input("enter list name >: ")
 m3u8ListName += ".m3u8"
 m3u8ListFullName += m3u8ListName
 
-# print(m3u8ListFullName)
-
 
 mp3u9file = open(m3u8ListFullName,"w+")
-mp3u9file.write(listContent)
+mp3u9file.write(finalListTrack)
 mp3u9file.close()
 
-# print(m3u8ListName);
-
-input("Jobe Done Enjoy your list :):)")
-# print(listContent)
-# input("Czekam zebys zobaczyl co widzisz")
+input("Job Done Enjoy your list :):)")
